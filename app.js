d4c176c20472f64c7f7588b78fdbc5256a6ee595
Husam <husamhk1@gmail.com>
2024-01-07 17:39:10 +0100

    npm i

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-
-const saltRounds = 10;
-
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 const app = express();
 
 
 app.use(express.static("public"));
 app.set('view engine' , 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(session({
+    secret: 'keyboard catOur little secret.',
+    resave: false,
+    saveUninitialized: true,
+    //cookie: { secure: true }
+  }))
 
 mongoose.connect("mongodb://localhost:27017/UserDB").then(()=>{
     console.log("connection successfully!")
@@ -41,33 +47,9 @@ app.get("/login", function(req,res){
 });
 
 app.post("/login", (req,res)=>{
-   const username = req.body.username;
-   const password = req.body.password;
-
    
-    User.findOne({email:username}).then((user, err)=>{
-      if(err){
-        console.log(err);
-      }else{
-        bcrypt.compare(password, user.password, function(err1, result) {
-            // result == true
-            if(result == true){
-                console.log("Logged!");
-                res.render("secrets");
 
-            } else {
-                console.log(err1);
-            }           
-            
-        });
-      }
-         
-    }).catch((err)=>{
-        console.log(err);
-        console.log("The Acount does't exist!");
-    }) 
-
-})
+});
 
 app.get("/register", async (req,res) =>{
     res.render("register")
@@ -77,21 +59,6 @@ app.get("/register", async (req,res) =>{
 app.post("/register",  (req, res)=>{
 
 
-bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    // Store hash in your password DB.
-    const newUser = new User({
-        email: (req.body.username),
-        password: hash
-    });
-
-    newUser.save().then(()=>{
-        console.log("Saved!");
-        res.render("secrets");
-    }).catch((err)=>{
-        console.log(err);
-        console.log("Error");
-    }) 
-});
 
 
 });
